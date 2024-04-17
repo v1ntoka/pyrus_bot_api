4c945ee6acc062d2172abbc27a7f05f3b47c1a4e
v1ntoka <galimov64946@gmail.com>
2024-04-17 17:39:17 +0300

    спарсил все задачи по форме

---
+++ modules/client.py
@@ -0,0 +1,7 @@
+import requests
+from dotenv import load_dotenv
+import os
+import json
+
+class Client:
+    ...
--- modules/parser.py
+++ modules/parser.py
@@ -1,15 +1,26 @@
 import requests
 from auth import Authorisation
+import json
 
 
 class Forms:
-    def __init__(self) -> None:
-        self.access_token = Authorisation().access_token
+    def __init__(self, ) -> None:
+        self.headers = Authorisation().headers
+        url = "https://pyrus.sovcombank.ru/api/v4/forms/{form_id}/"
 
 
 def main():
     f = Forms()
-    print(f.access_token)
+    headers = f.headers
+    url = "https://pyrus.sovcombank.ru/api/v4/forms/{form_id}/register".format(
+        form_id=307143)
+    # url = "https://pyrus.sovcombank.ru/api/v4/forms"
+    response = json.loads(requests.get(
+        url=url, headers=headers, params={"include_archived": "y", "": ""}).text)
+    with open("output.json", 'w', encoding="utf-8") as output:
+        json.dump(response["tasks"], output, ensure_ascii=False)
+
+    print(len(response["tasks"]))
 
 
 if __name__ == "__main__":
